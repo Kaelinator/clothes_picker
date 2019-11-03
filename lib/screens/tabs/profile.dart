4cb2d5b4bac4a1764c1df8ef4810e81d9276fa3f
Emilio <erogalla@utexas.edu>
2019-11-03 01:07:40 -0600

    Fixed Profile Buttons

--- lib/screens/tabs/profile.dart
+++ lib/screens/tabs/profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clothes_picker/screens/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 class ProfileView extends StatelessWidget {
   final String _fullName = "Randy Kirk";
@@ -142,10 +144,48 @@ class ProfileView extends StatelessWidget {
     );
   }
 
+  void _showDialog(BuildContext context) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Are you really washing your clothes?"),
+          content: new Text("Confirming means you will reset your worn clothes for this week."),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Back"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Confirm"),
+              onPressed: () {
+                _resetWardrobe();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _resetWardrobe() {
+    print("Resetting Wardrobe");
+    return;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance =
+      ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -157,18 +197,47 @@ class ProfileView extends StatelessWidget {
                   _buildStatContainer(),
                   _buildSeparator(screenSize),
                   SizedBox(height: 8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Wardrobe", style: getTextStyle()),
-                      InkWell(
-                        onTap: () => print("add article"),
-                        child: Icon(Icons.add_circle),
-                      ),    
-                    ],
+                  Text("Wardrobe", style: getTextStyle()),
+                  CategoryName("Hats"),
+                  CategoryName("Tops"),
+                  CategoryName("Bottoms"),
+                  CategoryName("Shoes"),
+                  CategoryName("Accessories"),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(60),
                   ),
-                  SizedBox(height: 4.0),
-                  SizedBox(height: 8.0),
+                  InkWell(
+                    child: Container(
+                      width: ScreenUtil.getInstance().setWidth(600),
+                      height: ScreenUtil.getInstance().setHeight(100),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Color(0xFFFF0000),
+                            Color(0xFFA13D2D)
+                          ]),
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xFF6078ea).withOpacity(.3),
+                                offset: Offset(0.0, 8.0),
+                                blurRadius: 8.0)
+                          ]),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () =>_showDialog(context),
+                          child: Center(
+                            child: Text("Wash Clothing (Reset)",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "Poppins-Bold",
+                                    fontSize: 16,
+                                    letterSpacing: 1.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                   _buildButtons(),
                 ],
               ),
@@ -191,3 +260,57 @@ class ProfileView extends StatelessWidget {
 
     return _nameTextStyle;
  }
+
+class CategoryName extends StatelessWidget {
+  final String categoryName;
+
+  const CategoryName(this.categoryName);
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance =
+      ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return Container(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+              height: ScreenUtil.getInstance().setHeight(60),
+            ),
+          InkWell(
+            child: Container(
+              width: ScreenUtil.getInstance().setWidth(600),
+              height: ScreenUtil.getInstance().setHeight(100),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xFF17ead9),
+                    Color(0xFF6078ea)
+                  ]),
+                  borderRadius: BorderRadius.circular(6.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color(0xFF6078ea).withOpacity(.3),
+                        offset: Offset(0.0, 8.0),
+                        blurRadius: 8.0)
+                  ]),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  // onTap: () => Navigator.pushNamed(context, categoryName),
+                  child: Center(
+                    child: Text(categoryName,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Poppins-Bold",
+                            fontSize: 16,
+                            letterSpacing: 1.0)),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]
+      )
+    );
+  }
+}
