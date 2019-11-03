c553337b749a6a6d7792baecab45f7b48fab6284
Shan Memon <shankmemon@gmail.com>
2019-11-02 23:11:40 -0500

    started fit creator

--- lib/screens/tabs/createfit.dart
+++ lib/screens/tabs/createfit.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
 
 class FitCreaterView extends StatelessWidget {
+
+
+  Widget _clothSlot(String title, ImageProvider<dynamic> img){
+    return Container(
+      padding: const EdgeInsets.all(8),
+      
+      child: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(title),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: img,
+                    fit: BoxFit.cover
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0.0, 15.0),
+            blurRadius: 15.0
+          ),
+            BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0.0, -10.0),
+            blurRadius: 15.0
+          ),
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -10,38 +53,16 @@ class FitCreaterView extends StatelessWidget {
           SliverPadding(
             padding: const EdgeInsets.all(20),
             sliver: SliverGrid.count(
-              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
               children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('He\'d have you all unravel at the'),
-                  color: Colors.grey,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Heed not the rabble'),
-                  color: Colors.green[200],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Sound of screams but the'),
-                  color: Colors.green[300],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Who scream'),
-                  color: Colors.green[400],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Revolution is coming...'),
-                  color: Colors.green[500],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Revolution, they...'),
-                  color: Colors.green[600],
-                ),
+                _clothSlot("Hat", AssetImage('assets/baseball_default.png')),
+                _clothSlot("Shirt",AssetImage('assets/shirt_default.jpg') ),
+                _clothSlot("Accessory",AssetImage('assets/add.jpg') ),
+                _clothSlot("Pants", AssetImage('assets/pants_default.png')),
+                _clothSlot("Accessory",AssetImage('assets/add.jpg') ),
+                _clothSlot("Shoes",AssetImage('assets/shoes_default.png') ),
               ],
             ),
           ),
