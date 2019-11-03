aeff5ef9ce883fb7311538d7e53ea1ee9f41531b
Kael <kirkkael@gmail.com>
2019-11-02 20:11:04 -0500

    getting photo works, finally

--- lib/screens/create-article.dart
+++ lib/screens/create-article.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 const List<String> CLOTHING_TYPES = <String>[
+  'A',
+  'B',
+  'C'
+];
+
+const List<String> WARMTH_TYPES = <String>[
   'A',
   'B',
   'C'
@@ -26,13 +35,28 @@ class CreateArticle extends StatefulWidget {
 class _CreateArticleState extends State<CreateArticle> {
   
   final _formKey = GlobalKey<FormState>();
-  // final email = TextEditingController();
+  final _name = TextEditingController();
+  bool _isRainFriendly = false;
   String _type;
+  String _warmth;
   String _errorText;
+  File _image;
 
   void _setType(String type) {
     setState(() {
       _type = type;
+    });
+  }
+
+  void _setWarmth(String warmth) {
+    setState(() {
+      _warmth = warmth;
+    });
+  }
+
+  void _setIsRainFriendly(bool isRainFriendly) {
+    setState(() {
+      _isRainFriendly = isRainFriendly;
     });
   }
 
@@ -44,10 +68,18 @@ class _CreateArticleState extends State<CreateArticle> {
       return;
     }
 
+    setState(() {
+      _errorText = null;
+    });
+
     Firestore.instance.collection('articles')
       .document()
       .setData({
-        'type': _type
+        // 'image': ,
+        'name': _name.text,
+        'type': _type,
+        'warmth': _warmth,
+        'isRainFriendly': _isRainFriendly
       })
       .then((_) {
         print('created article of clothing');
@@ -67,13 +99,32 @@ class _CreateArticleState extends State<CreateArticle> {
     return Text('$_errorText', style: TextStyle(color: Colors.redAccent, fontSize: 20));
   }
 
+  void _addImage() {
+    
+    ImagePicker.pickImage(source: ImageSource.gallery)
+      .then((File image) {
+        setState(() {
+          _image = image;
+        });
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Form(
+        key: _formKey,
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
+              TextFormField(
+                controller: _name,
+                decoration: InputDecoration(labelText: 'Name')
+              ),
+              MaterialButton(
+                child: Text('${_image == null ? 'Add' : 'Change' } photo'),
+                onPressed: _addImage,
+              ),
               Row(
                 children: <Widget>[
                   const Text('Type:'),
@@ -87,6 +138,30 @@ class _CreateArticleState extends State<CreateArticle> {
                     }).toList(),
                     onChanged: _setType,
                   )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  const Text('Warmth:'),
+                  DropdownButton<String>(
+                    value: _type,
+                    items: WARMTH_TYPES.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: _setWarmth,
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Checkbox(
+                    value: _isRainFriendly,
+                    onChanged: _setIsRainFriendly,
+                  ),
+                  const Text('Rain friendly'),
                 ],
               ),
               _showError(),
